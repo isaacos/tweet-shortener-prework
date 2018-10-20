83d7c38b8126fd4fe019caf6d1ab0783690792cf
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-20 03:40:19 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,12 +13,14 @@ end
 
 def word_substituter (tweet)
   words_array = tweet.split(' ')
+  new_array = []
   
   words_array.each do |word|
     if dictionary.keys.include? (word)
-      word = dictionary['word']
-      puts word
+      new_array << dictionary[word]
+    else 
+      new_array << word
     end
   end 
-  words_array.join(' ')
+  new_array.join(' ')
 end
