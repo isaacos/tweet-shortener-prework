7318a8a5fe1d33df8bfa51f5339c8a01cf0d1020
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-20 04:07:38 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,5 +35,10 @@ def bulk_tweet_shortener(array_of_tweets)
   array_of_tweets
 end
 
-def selective_tweet_shortener ()
+def selective_tweet_shortener (tweet)
+  if tweet.length > 140
+   return word_substituter(tweet)
+  else
+    return tweet
+  end
 end
