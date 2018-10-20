d2f570729733f23bbcb8a42c53a2730d6eb9087d
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-20 03:35:21 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,13 +1,13 @@
 # Write your code here.
 def dictionary
-  {'hello': 'hi',
-    'to': '2',
-    'two': '2',
-    'too': '2',
-    'be': 'b',
-    'you': 'u',
-    'at': '@',
-    'and': '&'
+  {'hello'=> 'hi',
+    'to'=> '2',
+    'two'=> '2',
+    'too' => '2',
+    'be' => 'b',
+    'you'=> 'u',
+    'at'=> '@',
+    'and'=> '&'
   }
 end
 
@@ -15,8 +15,9 @@ def word_substituter (tweet)
   words_array = tweet.split(' ')
   
   words_array.each do |word|
-    if 
+    if dictionary.keys.include? (word)
+      word = dictionary['word']
     end
   end 
-  
+  words_array.join(' ')
 end
